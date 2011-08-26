4b5fbc060ad1a0baaa461a2a0252ad1fcd8366a1
Guillaume Aubert <guillaume.aubert@gmail.com>
2011-08-26 15:35:38 +0200

    add upload

--- imgr-core/imgr/web/flask_server.py
+++ imgr-core/imgr/web/flask_server.py
@@ -3,20 +3,54 @@ Created on Aug 23, 2011
 
 @author: guillaume.aubert@gmail.com
 '''
-
-from flask import g, Flask, render_template, request, redirect, flash, url_for, jsonify
-
+import os
+from flask import g, Flask, render_template, request, redirect, flash, url_for, jsonify, send_from_directory
+from werkzeug import secure_filename
 from imgr.web.views.hello import hello
 
+UPLOAD_FOLDER = '/tmp/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER']      = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.register_blueprint(hello)
-#app.register_module(hello)
 
 #route index.html and default to product details page
 @app.route('/')
 @app.route('/index.html')
 def index():
     return redirect(url_for('static', filename='index.html'))
+
+
+# everything for the upload part
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+    
+@app.route('/get-upload/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
